7453468b880da36dd6c46f8201deeafdb359c3a5
jmthoel13 <84604890+lexdura@users.noreply.github.com>
2022-10-23 03:29:24 -0400

    Add files via upload
    Began work on the GUI

--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
 package blackjack;
 
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game 
 {
@@ -9,16 +12,17 @@ public class Game
 	static Player[] computers = null;
 	static Deck deck = new Deck();
 	static Dealer dealer = new Dealer();
+	static int chips;
+	static int numOfPlayers;
+	static String name;
 			
 	public static void main(String[] args)
 	{
-		int chips;
-		int numOfPlayers;
-		String name;
+		gameGUI();
 		
 		//Get information from the user
 		Scanner get = new Scanner(System.in);
-		System.out.print("Welcome to Blakjack. Please enter your Name: ");
+		System.out.print("Welcome to Blackjack. Please enter your Name: ");
 		name = get.nextLine();
 		System.out.print("Hello " + name + ". Please enter the starting chip amount: ");
 		chips = get.nextInt();
@@ -78,6 +82,96 @@ public class Game
 			//Post game. Adjust chip amounts, reset deck
 			deck.resetDeck();
 		}
+	}
+	
+	//Gets information from the user
+	public static void getInfo(JFrame f)
+	{
+		//Creating and framing all necessary items for the GUI
+		JDialog userInfo = new JDialog(f, "Who are you?", true);
+		userInfo.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		JPanel panel = new JPanel();
+		panel.setSize(450, 350);
+		panel.setLayout(null);
+		JLabel userName = new JLabel("Player name:");
+		userName.setBounds(20, 20, 100, 50);
+		JLabel startingPurse = new JLabel("Starting purse amount (500+):");
+		startingPurse.setBounds(20, 70, 150, 50);
+		JLabel computers = new JLabel("Additional Players (0-3): ");
+		computers.setBounds(20, 120, 150, 50);
+		JTextField getName = new JTextField();
+		getName.setBounds(200, 30, 200, 30);
+		JTextField getPurse = new JTextField();
+		getPurse.setBounds(200, 80, 200, 30);
+		JTextField getComputers = new JTextField();
+		getComputers.setBounds(200, 130, 200, 30);
+		JButton start = new JButton("START GAME");
+		start.setBounds(250, 190, 150, 50);
+		JButton cancel = new JButton("CANCEL");
+		cancel.setBounds(20, 190, 150, 50);
+		
+		//What the buttons do when clicked
+		start.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//Checks to make sure the user entered a valid amount in each field
+				if(Integer.parseInt(getPurse.getText()) > 499 && 
+				   Integer.parseInt(getComputers.getText()) >= 0 && 
+				   Integer.parseInt(getComputers.getText()) <= 3)
+				{
+					//Sets variables to user input
+					name = getName.getText();
+					chips = Integer.parseInt(getPurse.getText());
+					numOfPlayers = Integer.parseInt(getComputers.getText());
+					userInfo.dispose();
+				}
+			}
+		});
+		cancel.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				//Exits the JDialog box
+				userInfo.dispose();
+			}
+		});
+		
+		//Piecing the GUI together
+		userInfo.add(panel);
+		panel.add(userName);
+		panel.add(getName);
+		panel.add(startingPurse);
+		panel.add(getPurse);
+		panel.add(computers);
+		panel.add(getComputers);
+		panel.add(start);
+		panel.add(cancel);
+		
+		//Displays the GUI
+		userInfo.setSize(panel.getSize());
+		userInfo.setVisible(true);
+	}
+	
+	//Creates the GUI
+	public static void gameGUI()
+	{
+		JFrame game = new JFrame("G6 - Blackjack");
+		getInfo(game);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.setSize(1200, 800);
+        JPanel panel = new JPanel();
+        JLabel userName = new JLabel(name);
+        JLabel purse = new JLabel(Integer.toString(chips));
+        JLabel computers = new JLabel(Integer.toString(numOfPlayers));
+        
+        panel.add(userName);
+        panel.add(purse);
+        panel.add(computers);
+        game.add(panel);
+        
+        
+        game.setVisible(true);
 	}
 	
 	//Deals cards in a clockwise position starting with the "last" computer before ending at the dealer.
